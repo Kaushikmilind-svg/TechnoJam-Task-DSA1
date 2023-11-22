33c08a2648a525f49cb16957e3e7016c58cf661f
Kaushikmilind-svg <85938864+Kaushikmilind-svg@users.noreply.github.com>
2023-11-23 01:53:40 +0530

    Reversing_arr.py

--- Easy#1/Reversing_arr.py
+++ Easy#1/Reversing_arr.py
@@ -1,1 +1,17 @@
+def reverse_array(arr):
+    return arr[::-1]
 
+def reverse_string(st):
+    return st[::-1]
+
+#array
+array = input("Enter an array: ").split(',')
+reversed_array = reverse_array(array)
+print("Your Array:", array)
+print("Reversed Array:", reversed_array)
+
+#string
+string = str(input("Enter the string: "))
+reversed_string = reverse_string(string)
+print("Your String:", string)
+print("Reversed String:", reversed_string)
